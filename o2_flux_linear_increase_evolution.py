0ccc8555efe976a77f3c7158ccf557a2331ed9f4
Daniel Garduno Ruiz <dan@anahim.seos.uvic.ca>
2023-09-18 14:30:53 -0700

    fix missing parameter

--- o2_flux_linear_increase_evolution.py
+++ o2_flux_linear_increase_evolution.py
@@ -18,7 +18,8 @@ change_during_glaciations = eval(args.change_during_glaciations)
 # define surface temperature and O2 flux evolution
 time = np.arange(0, 500, 0.1)
 surf_temp_ev = surf_temp_evolution(time)
-o2_flux_ev = O2_flux_evolution_linear(time, o2_flux_gl2, o2_flux_gl3)
+o2_flux_ev = O2_flux_evolution_linear(time, o2_flux_gl2, o2_flux_gl3,
+    change_during_glaciations=change_during_glaciations)
 
 # run model
 chg_durung_gl_str = '_change_during_glaciations' if change_during_glaciations\
