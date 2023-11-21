97ba8fa20be1ef94b2349bf7ee5e3a49818be0d0
DanyIvan <dany_ivan_gr@hotmail.com>
2023-11-20 17:46:55 -0800

    add constant ri_flux evolution

--- o2_flux_constant_evolution.py
+++ o2_flux_constant_evolution.py
@@ -10,11 +10,12 @@ args = parser.parse_args()
 
 o2_flux = eval(args.o2_flux)
 
-# define surface temperature and O2 flux evolution
+# define surface temperature, O2 input flux, and reductant input flux evolution
 time = np.arange(0, 500, 0.1)
 surf_temp_ev = surf_temp_evolution(time)
 o2_flux_ev = np.repeat(o2_flux, len(time))
+ri_flux_ev = np.repeat(3.3e10, len(time))
 
 # run model
 name = 'o2_flux_constant_' + args.o2_flux
-run_model(name, time, o2_flux_ev, surf_temp_ev)
+run_model(name, time, o2_flux_ev, surf_temp_ev, ri_flux_ev)
--- o2_flux_linear_increase_evolution.py
+++ o2_flux_linear_increase_evolution.py
@@ -5,24 +5,25 @@ import argparse
 
 # define run parameters from terminal
 parser = argparse.ArgumentParser()
-parser.add_argument('--o2_flux_gl2', default='2.0e12')
+parser.add_argument('--o2_flux_0', default='1.0e12')
 parser.add_argument('--o2_flux_gl3', default='2.6e12')
 parser.add_argument('--change_during_glaciations', default='False')
 
 args = parser.parse_args()
 
-o2_flux_gl2 = eval(args.o2_flux_gl2)
+o2_flux_0 = eval(args.o2_flux_0)
 o2_flux_gl3 = eval(args.o2_flux_gl3)
 change_during_glaciations = eval(args.change_during_glaciations)
 
-# define surface temperature and O2 flux evolution
+# define surface temperature, O2 input flux, and reductant input flux evolution
 time = np.arange(0, 500, 0.1)
 surf_temp_ev = surf_temp_evolution(time)
-o2_flux_ev = O2_flux_evolution_linear(time, o2_flux_gl2, o2_flux_gl3,
+o2_flux_ev = O2_flux_evolution_linear(time, o2_flux_0, o2_flux_gl3,
     change_during_glaciations=change_during_glaciations)
+ri_flux_ev = np.repeat(3.3e10, len(time))
 
 # run model
 chg_durung_gl_str = '_change_during_glaciations' if change_during_glaciations\
     else ''
 name = 'linear_o2_flux_increase' + chg_durung_gl_str
-run_model(name, time, o2_flux_ev, surf_temp_ev)
+run_model(name, time, o2_flux_ev, surf_temp_ev, ri_flux_ev)
